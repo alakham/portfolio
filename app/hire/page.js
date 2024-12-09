5349623db32ffd02d44b10d9b1b4c558099d3b05
alakham <makha2007@gmail.com>
2024-12-09 13:56:05 +0000

    updates metadata

--- app/hire/page.js
+++ app/hire/page.js
@@ -1,8 +1,13 @@
+import { Link } from "lucide-react";
+
 function Page() {
   return (
-    <button className="inline-flex items-center justify-center rounded-full whitespace-nowrap text-primary-900 bg-accent-300">
+    <Link
+      href="/contact"
+      className="inline-flex items-center justify-center rounded-full whitespace-nowrap text-primary-900 bg-accent-300"
+    >
       Hire me
-    </button>
+    </Link>
   );
 }
 
